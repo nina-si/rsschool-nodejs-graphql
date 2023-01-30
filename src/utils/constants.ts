3ffcdd3e775c43ec86b464682651642d56a77677
Nina Sitaeva <sitaeva.nn@gmail.com>
2023-01-30 01:07:56 +0100

    feat: posts endpoints resolvers

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,5 @@ export const ERRORS = {
   USER_NOT_SUBSCRIBED: 'User is not subscribed to provided userID',
   USER_ALREADY_SUBSCRIBED: 'User is already subscribed to provided userID',
   MEMBERTYPE_NOT_FOUND: 'Member-type not found',
+  POST_NOT_FOUND: 'Post not found',
 };
